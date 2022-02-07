2a61520f2f8c74c91c1207ff712fb663bfa26806
Marcus Oliveira <MARKUSVI17@GMAIL.COM>
2022-02-07 18:10:23 -0300

    Fixed addOperator function

--- assets/scripts/calculadora.js
+++ assets/scripts/calculadora.js
@@ -1,25 +1,26 @@
 let operator = "";
 let n1 = "";
 let n2 = "";
+let res = "";
 
 function calculate(event) {
   event.preventDefault();
   if (n1 !== "" && n2 !== "" && operator !== "") {
     switch (operator) {
       case "+":
-        res = 0 + parseInt(n1) + parseInt(n2)
+        res = parseInt(n1) + parseInt(n2)
         break;
 
       case "-":
-        res = 0 + parseInt(n1) - parseInt(n2)
+        res = parseInt(n1) - parseInt(n2)
         break;
 
       case "*":
-        res = 0 + parseInt(n1) * parseInt(n2)
+        res = parseInt(n1) * parseInt(n2)
         break;
 
       case "/":
-        res = 0 + parseInt(n1) / parseInt(n2)
+        res = parseInt(n1) / parseInt(n2)
         break;
     
       default:
@@ -32,27 +33,31 @@ function calculate(event) {
     n2 = "";
     operator = "";
   }
-  
 }
 
 function erase() {
   n1 = "";
   n2 = "";
   operator = "";
+  res = "";
   document.getElementById("display").innerHTML = "";
 }
 
 function addNumber(number) {
+  if (res !== "" && operator === "") { //Fazer só depois de implementar o calculate
+    erase();
+  }
+
   if (operator === "") {
     document.getElementById("display").innerHTML += number;
-    n1 = document.getElementById("display").innerHTML;
+    n1 += number;
   } else if (operator !== "" && n2 === "") {
     document.getElementById("display").innerHTML = "";
     document.getElementById("display").innerHTML += number;
-    n2 = document.getElementById("display").innerHTML;
+    n2 = number;
   } else {
     document.getElementById("display").innerHTML += number;
-    n2 = document.getElementById("display").innerHTML;
+    n2 += number;
   }
 }
 
